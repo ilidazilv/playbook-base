ccc50e3553e6f28bbceeb2d694c71a4c871f9ad8
Andrii Gorichko <85357742+Andruwa808@users.noreply.github.com>
2026-01-26 02:16:46 +0200

    feat: added annotation NoArgConstructor to UuidFilters and BooleanFilters (#2)

--- src/main/java/dev/ilidaz/playbook/base/filtering/models/UuidFilters.java
+++ src/main/java/dev/ilidaz/playbook/base/filtering/models/UuidFilters.java
@@ -1,6 +1,7 @@
 package dev.ilidaz.playbook.base.filtering.models;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @SuperBuilder
 public class UuidFilters extends BaseFilters<UUID> implements Serializable {
 
